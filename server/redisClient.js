78c0720d7df3b9ed6be365176ee474abc2659a3f
José Francisco <josefranciscooliveirapimentel@gmail.com>
2025-04-09 11:43:05 -0300

    redisClient.js

---
+++ server/redisClient.js
@@ -0,0 +1,6 @@
+//OPÇÃO DE MODALIZAR
+const { createClient } = require('redis');
+const client = createClient({ url: process.env.REDIS_URL });
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+module.exports = client;
